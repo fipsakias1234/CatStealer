afff3d5eec9ae7cd93c77d35693a2fdeab805f4d
fivos kapidis <fivos7@hotmail.gr>
2025-03-19 20:39:35 +0100

    Initial Migration

---
+++ CatStealer.Infrastructure/Migrations/20250319193609_InitialCatEntityCreation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatStealer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCatEntityCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StolenCats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StolenCats", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StolenCats");
+        }
+    }
+}
